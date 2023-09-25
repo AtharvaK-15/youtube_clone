be6a1efa211a0f9ee938a1ad2aad8649dd36054e
atharva <atharvak.8023@gmail.com>
2023-09-26 00:17:59 +0530

    made components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+// box is nothing more just a simple div element
 import {Box} from '@mui/material'
+import Navbar from './components/Navbar';
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
 
 const App = () => {
-    return (
-     <div>Hello World</div>
-        );
+    <BrowserRouter>
+        <Box sx={{backgroundColor:'#000'}}>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact element={<Feed />}/>
+                <Route path='/video/:id' exact element={<VideoDetail />}/>
+                <Route path='/channel/:id' exact element={<ChannelDetail />}/>
+                <Route path='/search/:searchTerm' exact element={<SearchFeed />}/>
+            </Routes>
+        </Box>
+    </BrowserRouter>
     };
 
 export default App;
