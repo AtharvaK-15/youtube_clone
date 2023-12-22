984a5a65646245a7ba175c05a0ed7514487c7d52
atharva <atharvak.8023@gmail.com>
2023-12-22 15:55:25 +0530

    search bar and search feed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,8 +4,18 @@ import { Paper, IconButton, Icon } from '@mui/material'
 import {Search} from '@mui/icons-material'
 
 const SearchBar = () => {
+   const [searchTerm, setSearchTerm] = useState('')
+   const navigate = useNavigate()
+   
+   const handleSubmit=(e)=>{
+      e.preventDefault()
+      if(searchTerm){
+         navigate(`/search/${searchTerm}`)
+      }
+      setSearchTerm('')
+   }
   return (
-     <Paper component="form" onSubmit={()=>{}}
+     <Paper component="form" onSubmit={handleSubmit}
      sx={{
         borderRadius:20,
         border:'1px solid #e3e3e3',
@@ -17,8 +27,8 @@ const SearchBar = () => {
         <input 
             className='search-bar'
             placeholder='Search...'
-            value=""
-            onChange={()=>{}}
+            value={searchTerm}
+            onChange={(e)=>{setSearchTerm(e.target.value)}}
         />
         <IconButton type='submit' sx={{p:'10px',color:'red'}}>
             <Search/>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
+import Videos from './Videos'
+import { FetchFromAPI } from '../utils/FetchFromAPI'
+import { useParams } from 'react-router-dom'
 
 const SearchFeed = () => {
+
+  const [videos,setVideos] = useState([]);
+  const {searchTerm} = useParams();
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await FetchFromAPI(`search?part=snippet&q=${searchTerm}`);
+        setVideos(data.items);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
+  }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY:"auto", height:'90vh', flex:'2'}}>
+      <Typography variant='h4' fontWeight='bold' mb='2' sx={{color:"White"}}>
+        Search Results for:  <span style={{color:"#F31503"}}>{searchTerm}</span> videos
+      </Typography>
+        <Videos videos={videos} />
+  </Box>
   )
 }
 
-export default SearchFeed
+export default SearchFeed;
